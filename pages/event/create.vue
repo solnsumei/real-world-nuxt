7e13520a864eb5979b25ba47ae84d097ab3bbe8e
solnsumei <solnsumei@gmail.com>
2020-04-17 06:55:00 +0100

    Updated app

---
+++ pages/event/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Create An Event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create an event in your neighborhood'
+        }
+      ]
+    };
+  }
+};
+</script>
